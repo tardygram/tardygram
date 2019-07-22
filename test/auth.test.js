c1f8a9a433fa6ec52d98ad43bc7ebeebc95809ad
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-22 14:51:06 -0700

    made post signin route and test

--- test/auth.test.js
+++ test/auth.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const User = require('../lib/models/User');
 
 describe('users routes', () => {
   beforeAll(() => {
@@ -12,6 +13,15 @@ describe('users routes', () => {
 
   beforeEach(() => {
     return mongoose.connection.dropDatabase();
+  });
+
+  let user;
+  beforeEach(async() => {
+    user = await User.create({
+      username: 'signin@test.com',
+      password: 'password',
+      profilePhotoUrl: 'http://test.jpeg'
+    });
   });
 
   afterAll(() => {
@@ -30,6 +40,23 @@ describe('users routes', () => {
         expect(res.body).toEqual({
           _id: expect.any(String),
           username: 'test@test.com',
+          profilePhotoUrl: 'http://test.jpeg',
+          __v: 0
+        });
+      });
+  });
+
+  it('signs in a user, returns user', () => {
+    return request(app)
+      .post('/api/v1/auth/signin')
+      .send({
+        username: user.username,
+        password: 'password'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          username: 'signin@test.com',
           profilePhotoUrl: 'http://test.jpeg',
           __v: 0
         });

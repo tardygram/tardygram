ad8573ba77f7b663e9fbdf864cc28d02baa42c6d
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-22 17:17:49 -0700

    deletes a post with DELETE (#5)

--- test/posts.test.js
+++ test/posts.test.js
@@ -5,6 +5,7 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const User = require('../lib/models/User');
+const Post = require('../lib/models/Post');
 
 describe('users routes', () => {
   beforeAll(() => {
@@ -17,6 +18,7 @@ describe('users routes', () => {
 
   let user;
   let token;
+  let post;
   beforeEach(async() => {
     user = await User.create({
       username: 'signin@test.com',
@@ -24,6 +26,14 @@ describe('users routes', () => {
       profilePhotoUrl: 'http://test.jpeg'
     });
     token = user.authToken();
+
+    post = await Post.create({
+      photoUrl: 'http://generic_photo.jpg',
+      user: user._id,
+      caption: 'Awesome pic!! Yay',
+      tags: ['cats', 'kittens']
+    });
+
   });
 
   afterAll(() => {
@@ -49,6 +59,16 @@ describe('users routes', () => {
           tags: ['dogs', 'puppies'],
           __v: 0
         });
+      });
+  });
+
+  it('deletes a post with DELETE', () => {
+    return request(app)
+      .delete(`/api/v1/posts/${post._id}`)
+      .set('Cookie', [`session=${token}`])
+      .then(res => {
+        const postJSON = JSON.parse(JSON.stringify(post));
+        expect(res.body).toEqual(postJSON);
       });
   });
 });

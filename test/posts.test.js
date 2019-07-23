49cda39dc88972fa696adc7fc73c0297104c51f1
Jackmc <jackmc1122@gmail.com>
2019-07-22 17:00:59 -0700

    Add GET all posts route.

--- test/posts.test.js
+++ test/posts.test.js
@@ -5,6 +5,7 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const User = require('../lib/models/User');
+const Post = require('../lib/models/Post');
 
 describe('users routes', () => {
   beforeAll(() => {
@@ -17,6 +18,7 @@ describe('users routes', () => {
 
   let user;
   let token;
+  let post;
   beforeEach(async() => {
     user = await User.create({
       username: 'signin@test.com',
@@ -24,6 +26,12 @@ describe('users routes', () => {
       profilePhotoUrl: 'http://test.jpeg'
     });
     token = user.authToken();
+    post = await Post.create({
+      photoUrl: 'http://generic_photo.jpg',
+      user: user._id,
+      caption: 'Awesome pic!! Yay',
+      tags: ['cats', 'kittens']
+    });
   });
 
   afterAll(() => {
@@ -47,6 +55,23 @@ describe('users routes', () => {
           user: user._id.toString(),
           caption: 'Awesome pic!!',
           tags: ['dogs', 'puppies'],
+          __v: 0
+        });
+      });
+  });
+
+  it('returns a list of all posts', () => {
+    return request(app)
+      .get('/api/v1/posts')
+      .set('Cookie', [`session=${token}`])
+      .then(res => {
+        expect(res.body).toEqual(expect.any(Array));
+        expect(res.body[0]).toEqual({
+          _id: expect.any(String),
+          photoUrl: expect.any(String),
+          user: expect.any(String),
+          caption: expect.any(String),
+          tags: expect.any(Array),
           __v: 0
         });
       });

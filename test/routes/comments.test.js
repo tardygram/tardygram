a3996cacc711212b47d162cdd5a13cc9d06dcbc8
Jackmc <jackmc1122@gmail.com>
2019-07-23 15:27:18 -0700

    create comment post route and test

---
+++ test/routes/comments.test.js
@@ -0,0 +1,118 @@
+const { getAgent, getPosts, getUsers, getComments } = require('../data-helpers');
+
+describe('comments routes', () => {
+  it('creates a new comment', () => {
+    const users = getUsers();
+    const posts = getPosts();
+
+    return getAgent()
+      .post('/api/v1/comments')
+      .send({
+        commentBy: users[0]._id,
+        post: posts[0]._id,
+        comment: 'Random comment'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          commentBy: users[0]._id.toString(),
+          post: posts[0]._id.toString(),
+          comment: 'Random comment',
+          __v: 0
+        });
+      });
+  });
+
+  // it('returns a post by its id', () => {
+  //   const posts = getPosts();
+  //   return getAgent()
+  //     .get(`/api/v1/posts/${posts[0]._id}`)
+  //     .then(res => {
+  //       expect(res.body).toEqual({
+  //         _id: posts[0]._id.toString(),
+  //         photoUrl: posts[0].photoUrl,
+  //         user: {
+  //           _id: posts[0].user,
+  //           profilePhotoUrl: expect.any(String),
+  //           username: expect.any(String),
+  //           __v: 0
+  //         },
+  //         caption: posts[0].caption,
+  //         tags: posts[0].tags,
+  //         __v: 0
+  //       });
+  //     });
+  // });
+
+  // it('returns a list of all posts', () => {
+  //   return getAgent()
+  //     .get('/api/v1/posts')
+  //     .then(res => {
+  //       expect(res.body).toEqual(expect.any(Array));
+  //       expect(res.body[0]).toEqual({
+  //         _id: expect.any(String),
+  //         photoUrl: expect.any(String),
+  //         user: expect.any(String),
+  //         caption: expect.any(String),
+  //         tags: expect.any(Array),
+  //         __v: 0
+  //       });
+  //     });
+  // });
+
+  // it('updates a post', () => {
+  //   const posts = getPosts();
+  //   return getAgent()
+  //     .patch(`/api/v1/posts/${posts[0]._id}`)
+  //     .send({
+  //       caption: 'Awesome pic!! Again.',
+  //     })
+  //     .then(res => {
+  //       expect(res.body).toEqual({
+  //         _id: posts[0]._id.toString(),
+  //         user: posts[0].user.toString(),
+  //         photoUrl: posts[0].photoUrl,
+  //         caption: 'Awesome pic!! Again.',
+  //         tags: posts[0].tags,
+  //         __v: 0
+  //       });
+  //     });
+  // });
+
+  // it('does not update a post when user did not create post', async() => {
+  //   const posts = getPosts();
+  //   return getAgent()
+  //     .patch(`/api/v1/posts/${posts[1]._id}`)
+  //     .send({
+  //       caption: 'Awesome pic!! Again.',
+  //     })
+  //     .then(res => {
+  //       expect(res.body).toEqual({
+  //         message: 'You are not logged in as the correct user',
+  //         status: 401
+  //       });
+  //     });
+  // });
+
+  // it('deletes a post with DELETE as the correct user', () => {
+  //   const posts = getPosts();
+  //   return getAgent()
+  //     .delete(`/api/v1/posts/${posts[0]._id}`)
+  //     .then(res => {
+  //       const postJSON = JSON.parse(JSON.stringify(posts[0]));
+  //       expect(res.body).toEqual(postJSON);
+  //     });
+  // });
+
+  // it('attempts to delete a post with DELETE as the incorrect user', () => {
+  //   const posts = getPosts();
+  //   return getAgent()
+  //     .delete(`/api/v1/posts/${posts[1]._id}`)
+  //     .then(res => {
+  //       expect(res.body).toEqual({
+  //         message: 'You are not logged in as the correct user',
+  //         status: 401
+  //       });
+  //     });
+  // });
+});

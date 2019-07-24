df73a4b2f68a856dc9158eb1576b0ec3ba5a605d
sepuckett86 <sepuckett86@gmail.com>
2019-07-24 10:11:58 -0700

    add test and route for users/prolific

---
+++ test/routes/prolific.test.js
@@ -0,0 +1,18 @@
+const { getAgent } = require('../data-helpers');
+
+describe('users routes', () => {
+  it('responds with 10 users with the most posts', () => {
+    return getAgent()
+      .get('/api/v1/users/prolific')
+      .then(res => {
+        expect(res.body).toHaveLength(10);
+        expect(res.body[0]).toEqual({
+          _id: expect.any(String),
+          username: expect.any(String),
+          profilePhotoUrl: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+});
+
